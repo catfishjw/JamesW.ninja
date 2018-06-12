d86ce1edeb0476c770247d302de8b058737b218e
catfishjw <39639229+catfishjw@users.noreply.github.com>
2018-06-12 16:00:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# JamesW.ninja
---
+++ projects/calculator/calculator.js
@@ -0,0 +1,19 @@
+// Determines button clicked via id
+function myFunction(id) {
+    document.calc.result.value+=id;
+  }
+  
+  // Clears calculator input screen
+  function clearScreen() {
+    document.calc.result.value="";
+  }
+  
+  // Calculates input values
+  function calculate() {
+    try {
+      var input = eval(document.calc.result.value);
+      document.calc.result.value=input;
+    } catch(err){
+        document.calc.result.value="Error";
+      }
+  }
